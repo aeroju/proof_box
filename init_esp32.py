bc0862b878484f8fe83d1e3455bf93702fa67b43
aeroju <aeroju@sina.com>
2021-12-31 16:04:47 +0800

    refactor project with more message control

--- init_esp32.py
+++ init_esp32.py
@@ -8,5 +8,5 @@ files = os.listdir(path)
 for f in files:
     if(os.path.splitext(f)[1] in ['.py','.html'] and f != os.path.split(__file__)[1]):
         print('sending {} to {}'.format(os.path.join(path,f),device))
-        args=['ampy.py','-p',device,'put',os.path.join(path,f)]
+        args=['ampy','-p',device,'put',os.path.join(path,f)]
         subprocess.run(args)
